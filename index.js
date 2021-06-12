99e40bab96ea7630b430cf5e02fce92028a77094
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-06-12 11:12:38 +0300

    get the day of birth on the week through indexing

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ var generateAkanName = () =>{
 
     let dayOfWeekNumber = Math.floor((((parseInt(yearBorn.slice(0,2))/4)-2*parseInt(yearBorn.slice(0,2))-1)+
             ((5*parseInt(yearBorn.slice(2,4))/4))+((26*(monthBorn+1)/10))+dayBorn)%7);
-            
+
     let akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    let akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];      
+    let akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];     
+    
+    let indexOfArrays;    
+    if (dayOfWeekNumber == 0){
+        indexOfArrays = 0;
+    } else {
+        indexOfArrays = dayOfWeekNumber;
+    }
+  
