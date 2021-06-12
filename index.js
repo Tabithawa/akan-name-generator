852ec58763bbb5c1fe65ea9dad488809a35de275
Tabithawa <85102745+Tabithawa@users.noreply.github.com>
2021-06-12 19:48:58 +0300

    Add functions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var generateAkanName = () =>{
+function generateAkanName() {
     let dayBorn = parseInt(document.getElementById("dayInput").value);
     let monthBorn = parseInt(document.getElementById("monthInput").value);
     let yearBorn = document.getElementById("yearInput").value;
     
     let genderChoosing = document.getElementsByName("gender");
-    var selectGender= () => {
+    function selectGender() {
         for (let gender of genderChoosing){
             if (gender.checked){
                 return gender.value;
