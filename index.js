46c3c788f7156e05b8cef2702f6075234fbe2c26
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-06-12 11:10:12 +0300

    get gender and calculate day born

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ var generateAkanName = () =>{
         }
     }      
 }
+    let genderSelected = selectGender();
+
+    let dayOfWeekNumber = Math.floor((((parseInt(yearBorn.slice(0,2))/4)-2*parseInt(yearBorn.slice(0,2))-1)+
+            ((5*parseInt(yearBorn.slice(2,4))/4))+((26*(monthBorn+1)/10))+dayBorn)%7);
+            
