6719bcfb9f5c8753f00abb4bd400a25abd0717d8
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-06-12 11:11:41 +0300

    list akan names in arrays

--- index.js
+++ index.js
@@ -16,3 +16,5 @@ var generateAkanName = () =>{
     let dayOfWeekNumber = Math.floor((((parseInt(yearBorn.slice(0,2))/4)-2*parseInt(yearBorn.slice(0,2))-1)+
             ((5*parseInt(yearBorn.slice(2,4))/4))+((26*(monthBorn+1)/10))+dayBorn)%7);
             
+    let akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];      
